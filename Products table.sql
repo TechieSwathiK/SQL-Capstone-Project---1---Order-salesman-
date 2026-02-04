729fc95f92e3311d64266e20be7106c9f35327d1
TechieSwathiK <swathi.codingal@gmail.com>
2026-02-04 14:48:36 +0530

    Create Products table.sql

---
+++ Products table.sql
@@ -0,0 +1,43 @@
+CREATE TABLE IF NOT EXISTS PRODUCTS (
+    PRODUCT_ID TEXT,
+    PRODUCT_NAME TEXT,
+    SUPPLIER_ID TEXT,
+    CATEGORY_ID TEXT,
+    UNIT TEXT,
+    PRICE REAL
+);
+
+
+INSERT INTO PRODUCTS  VALUES ('1', 'CHAIS', '1', '1', '10 BOXES*20 BAGS', 18);
+INSERT INTO PRODUCTS  VALUES ('2', 'CHANG', '1', '1', '24-12 OZ BOTTLES', 19);
+INSERT INTO PRODUCTS  VALUES ('3', 'ANISEED SYRUP', '1', '2', '12-550 ML BOTTLES', 10);
+INSERT INTO PRODUCTS  VALUES ('4', 'CHEF ANTON SEASONING', '2', '2', '48-6 OZ JARS', 22);
+INSERT INTO PRODUCTS  VALUES ('5', 'CHEF ANTON MIX', '2', '2', '36 BOXES', 21.35);
+
+
+SELECT * FROM  PRODUCTS;
+
+
+
+-- Query to count the number of products
+SELECT COUNT(PRODUCT_ID) AS Product_Count
+FROM PRODUCTS;
+
+-- Query to find the average price of products
+SELECT AVG(PRICE) AS Average_Price
+FROM PRODUCTS;
+
+
+-- Query to find the total price of products
+SELECT MAX(PRICE) AS MAXIMUM
+FROM PRODUCTS;
+
+select min(PRICE) as minimum
+from PRODUCTS;
+
+
+SELECT SUM(PRICE) AS Total_price
+FROM PRODUCTS;
+
+
+
